bc19ccd4fe4e9b00a7957bd0ba0159670ccad092
Santhosh Kumar L <lsanthoshkumar01@gmail.com>
2023-10-29 23:44:01 +0530

    stream commit

---
+++ SteamAPI/src/Steam/Filters.java
@@ -0,0 +1,21 @@
+package Steam;
+import java.util.*;
+public class Filters {
+
+    public static void main(String[] args) {
+        List<String> list=new ArrayList<>();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of strings:");
+        int n=sc.nextInt();
+        System.out.println("Enter the strings:");
+        for(int i=1;i<= n;i++) 
+        {
+            String s=sc.next();
+            list.add(s);
+        }
+        System.out.println("Strings after removing:");
+        list.stream().filter(s ->s.length()>5).forEach(System.out::println);
+    }
+}
+
+
